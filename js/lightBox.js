e428c40f3b157ce8c760a304c8163d34562f34fe
Amr-shoeib <amr79.a@gmail.com>
2023-03-13 21:48:56 -0400

    css update

--- js/lightBox.js
+++ js/lightBox.js
@@ -1,14 +1,14 @@
 
  const dataDisplay = document.getElementById("cards-wrapper");
- debugger;
+//  debugger;
  const button = document.getElementById('lightboxButton');
  const lightbox = document.querySelector('.lightbox');
  const closeLightbox = document.querySelector('.close-lightbox');
 
- debugger;
+//  debugger;
 
  dataDisplay.addEventListener('click', function(event) {
-  debugger;
+//   debugger;
   lightbox.style.display = 'block';
 }, false);
 
@@ -28,16 +28,31 @@ button.addEventListener('click', () => {
   fetch('data.json')
   .then(response => response.json())
   .then(data => {
+    
 
     // Display data on the web page
     // dataDisplay.innerHTML = JSON.stringify(data);
-    dataDisplay.innerHTML = JSON.stringify(data);
+    (dataDisplay.innerHTML = `
+    <div>
+    <h2 class="lightBox1">${data.name} </h2>
+    <h3> ${data.title}</h3>
+    <p>${data.story}</p>
+    </div>
+    ` );
+    
     
     })
+
+    
     .catch(error => {
       console.error('Error fetching data:', error);
     });
+
+    
+    
 });
+
+
 
 window.onload = () => {
   setTimeout(() => {
